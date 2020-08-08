0c5b748643df1306716e8766d232d3e2b7e66085
AliGhadirii <ali.ghadiri78>
2020-08-08 20:42:31 +0430

    Done P5

--- P5.py
+++ P5.py
@@ -1,1 +1,16 @@
-print("done")
+def gcd(a, b):
+    if (a == 0):
+        return b
+    return gcd(b % a, a)
+
+def LCM(a, b):
+    return (a * b) / gcd(a, b)
+
+def P5():
+    ans = 1
+    for i in range(1,21):
+        ans = LCM(ans, i)
+    return ans
+
+print(P5())
+
